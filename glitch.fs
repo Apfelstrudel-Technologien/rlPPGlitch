15ff8a64cf9505d37f7407f6d13cdf8e24db4f77
Ivan <94743980+xzripper@users.noreply.github.com>
2024-06-02 14:58:09 +0300

    Minify shader a little.
    Minify shader a little.

--- glitch.fs
+++ glitch.fs
@@ -17,13 +17,9 @@ void main(void) {
 
     uv.y = 1.0 - uv.y;
 
-    float rOffset = sin(time * tMult) * chromaMult;
-    float gOffset = cos(time * tMult) * chromaMult;
-    float bOffset = sin(time * tMult + 0.5) * chromaMult;
-
     float glitch = fract(uv.y * 32.0 + time * tMult) * gMult;
 
-    fragColor = vec4(texture(texture0, uv - vec2(rOffset + glitch * 2.0, 0.0)).r,
-                    texture(texture0, uv - vec2(gOffset + glitch * 1.5, 0.0)).g,
-                    texture(texture0, uv - vec2(bOffset + glitch, 0.0)).b, 1.0);
+    fragColor = vec4(texture(texture0, uv - vec2(sin(time * tMult) * chromaMult + glitch * 2.0, 0.0)).r,
+                    texture(texture0, uv - vec2(cos(time * tMult) * chromaMult + glitch * 1.5, 0.0)).g,
+                    texture(texture0, uv - vec2(sin(time * tMult + 0.5) * chromaMult + glitch, 0.0)).b, 1.0);
 }
